bd60dcae83304c671d6aff1466177b166effdb85
abed-404 <asafeiya2018@gmail.com>
2022-08-09 16:28:47 +0300

    list box needed

---
+++ src/handlers.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const path = require('path');
+
+const handleHomeRoute = (request, response) => {
+    const filePath = path.join(__dirname, '..', 'public', 'index.html');
+    fs.readFile(filePath, (error, file) => {
+        if (error) {
+            console.log(error);
+            response.writeHead(500, { 'Content-Type': 'text/html' });
+            response.end("<h1>Sorry, we've had a problem on our end</h1>");
+        } else {
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.end(file);
+        }
+    });
+};
+
+const handlePublic = (request, response, url) => {
+    const extension = url.split('.')[1];
+    const extensionType = {
+        html: 'text/html',
+        css: 'text/css',
+        js: 'application/javascript',
+        ico: 'image/x-icon',
+    };
+
+    const filePath = path.join(__dirname, '..', url);
+    fs.readFile(filePath, (error, file) => {
+        if (error) {
+            console.log(error);
+            response.writeHead(404, { 'Content-Type': 'text/html' });
+            response.end('<h1>404 file not found</h1>');
+        } else {
+            response.writeHead(200, { 'Content-Type': extensionType[extension] });
+            response.end(file);
+        }
+    });
+    console.log(url);
+};
+
+const getSuggestions = (key, response) => {
+    const file = fs.readFile('./src/countries.json', (error, data) => {
+        if (error) {
+            console.log(error);
+            response.writeHead(500, { 'Content-Type': 'text/html' });
+            response.end("<h1>Sorry, we've had a problem on our end</h1>");
+        } else {
+            const countries = JSON.parse(data.toString());
+            let matches = countries.filter(country => {
+                const regex = new RegExp(`^${key}`, 'gi');
+                return country.name.match(regex) || country.code.match(regex);
+            });
+            
+            if (key.length === 0 || matches.length == 0) {
+                response.writeHead(404);
+                response.end()
+            }
+            else {
+                response.writeHead(200, { 'Content-Type': 'application/json' });
+                response.end(JSON.stringify(matches))
+            }
+
+        }
+    })
+
+}
+module.exports = {
+    handleHomeRoute,
+    handlePublic,
+    getSuggestions
+};
---
+++ src/server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const router = require('./router')
+const port = 3000;
+
+const server = http.createServer(router);
+server.listen(port);
+
+console.log(`server up and running on localhost:${port}`);
+
